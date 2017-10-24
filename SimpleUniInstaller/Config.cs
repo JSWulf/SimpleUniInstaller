785995e470f0a8c3f1613c0c8a0cd33d0e797871
Joseph Wulf <JSW@JSWulf.com>
2017-10-24 16:59:22 -0700

    config class add

---
+++ SimpleUniInstaller/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleUniInstaller
+{
+    public class Config
+    {
+        public Config(string file)
+        {
+            ReadFile(file);
+        }
+
+        public string CompanyName { get; private set; }
+        public string SoftwareName { get; private set; }
+        public string InstallDir { get; private set; }
+
+        private void ReadFile(string file)
+        {
+
+        }
+    }
+}
